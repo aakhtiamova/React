ecd50c497de0067ae439e2e8f7330956c27542ea
Anastasiia Akhtiamova <aantonishen@gmail.com>
2025-07-18 14:49:31 +0200

    add properties to the component App

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,10 +1,12 @@
-const App = () => {
-  const [buttonText, setButtonText] = React.useState("Click me"); //array destructuring
-  const [classesList, setClassesList] = React.useState("");
+const App = ({ initialButtonText, initialClassesList }) => {
+  const [buttonText, setButtonText] = React.useState(initialButtonText); //array destructuring
+  const [classesList, setClassesList] = React.useState(initialClassesList);
+
   const onButtonClick = () => {
     setButtonText("Hello from React");
     setClassesList("green-btn");
   };
+
   return (
     <div className="app">
       <button className={classesList} onClick={onButtonClick}>
@@ -16,4 +18,4 @@ const App = () => {
 
 const container = document.getElementById("app");
 const root = ReactDOM.createRoot(container);
-root.render(<App />); // user componemt
+root.render(<App initialButtonText="Click me" initialClassesList="" />); // user componemt
