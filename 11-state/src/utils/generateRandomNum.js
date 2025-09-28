6e18f56570238adfbc00b79b0216a30e7b3b5906
Anastasiia Akhtiamova <aantonishen@gmail.com>
2025-09-28 15:40:43 +0200

    created project generateRandomNum

---
+++ 11-state/src/utils/generateRandomNum.js
@@ -0,0 +1,5 @@
+function generateRandomNum(maxNum) {
+  return Math.floor(Math.random() * maxNum); // default method random for return random number from 0 to 100. math.floor used for make integers numbers
+}
+
+export default generateRandomNum;
