ab382067ee2ca90e541d37ea158a7d74d387a888
Anastasiia Akhtiamova <aantonishen@gmail.com>
2025-10-10 16:29:20 +0200

    created project with React fragment

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,12 @@
+const Card = () => {
+  return (
+    // react fragment starts down
+    <>
+      <h1>Anastasiia</h1>
+      <h3>I am stading React</h3>
+      <button>Like!</button>
+    </>
+  );
+};
+
+export default Card;
