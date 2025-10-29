fdbd1745df90109583d9d1837bbda2d22666f0d5
Anastasiia Akhtiamova <aantonishen@gmail.com>
2025-10-29 15:21:24 +0100

    created project with posts

---
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+import Post from "./Post";
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => setPosts(posts))
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (error) {
+    return <h1>Error: {error} </h1>;
+  }
+
+  return (
+    <div>
+      <h1>Posts</h1>
+
+      <hr />
+
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        posts.map((post) => <Post key={post.id} {...post} />)
+      )}
+    </div>
+  );
+}
+
+export default Posts;
