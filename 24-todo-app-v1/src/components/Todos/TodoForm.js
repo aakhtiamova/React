581632e0f41875a70d42250c683b117d6fe53ee6
Anastasiia Akhtiamova <aantonishen@gmail.com>
2025-11-25 15:29:39 +0100

    created controlled input field for adding todos

--- 24-todo-app-v1/src/components/Todos/TodoForm.js
+++ 24-todo-app-v1/src/components/Todos/TodoForm.js
@@ -1,7 +1,19 @@
-function TodoForm() {
+import { useState } from "react";
+
+function TodoForm({ addTodo }) {
+  const [text, setText] = useState("");
+  const onSubmitHandler = (event) => {
+    event.preventDefault(); //don't open new window after submit
+    addTodo(text); // added new todo
+    setText(""); //clear input field after clicking on submit button
+  };
   return (
-    <form>
-      <input placeholder="Enter new todo" />
+    <form onSubmit={onSubmitHandler}>
+      <input
+        placeholder="Enter new todo"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <button type="submit">Submit</button>
     </form>
   );
