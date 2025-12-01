ab5315ccaa2eb7e9f4c5fca28dc9487a39ed3b2e
Anastasiia Akhtiamova <aantonishen@gmail.com>
2025-12-01 15:23:32 +0100

    added functional for showing message if there are no todos

--- 24-todo-app-v1/src/components/Todos/TodoList.js
+++ 24-todo-app-v1/src/components/Todos/TodoList.js
@@ -4,6 +4,7 @@ function TodoList(props) {
   const { todos } = props;
   return (
     <div className={styles.TodoListContainer}>
+      {todos.length === 0 && <h2>Todo list is empty</h2>}
       {todos.map((todo, index) => (
         <Todo key={index} todo={todo} />
       ))}
