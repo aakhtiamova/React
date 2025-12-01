1b8006c663c5f9617ba8d6bce8e32cebdab42439
Anastasiia Akhtiamova <aantonishen@gmail.com>
2025-12-01 15:09:46 +0100

    added styles for Todo and TodoList  components

--- 24-todo-app-v1/src/components/Todos/Todo.js
+++ 24-todo-app-v1/src/components/Todos/Todo.js
@@ -1,4 +1,9 @@
+import styles from "./Todo.module.css";
 function Todo({ todo }) {
-  return <h3>{todo}</h3>;
+  return (
+    <div className={styles.todo}>
+      <div className={styles.todoText}>{todo}</div>
+    </div>
+  );
 }
 export default Todo;
--- 24-todo-app-v1/src/components/Todos/TodoList.js
+++ 24-todo-app-v1/src/components/Todos/TodoList.js
@@ -1,8 +1,14 @@
 import Todo from "./Todo";
-
+import styles from "./TodoList.module.css";
 function TodoList(props) {
   const { todos } = props;
-  return todos.map((todo, index) => <Todo key={index} todo={todo} />);
+  return (
+    <div className={styles.TodoListContainer}>
+      {todos.map((todo, index) => (
+        <Todo key={index} todo={todo} />
+      ))}
+    </div>
+  );
 }
 
 export default TodoList;
