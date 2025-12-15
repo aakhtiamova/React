4f4c47cf97c4fbbfb59ab7c1ce53067f1225164b
Anastasiia Akhtiamova <aantonishen@gmail.com>
2025-12-15 15:42:14 +0100

    added toggleTodoHandler function

--- 25-todo-app-v2/src/components/Todos/TodoList.js
+++ 25-todo-app-v2/src/components/Todos/TodoList.js
@@ -1,12 +1,17 @@
 import Todo from "./Todo";
 import styles from "./TodoList.module.css";
-function TodoList({ todos, deleteTodo }) {
+function TodoList({ todos, deleteTodo, toggleTodo }) {
   return (
     <div className={styles.TodoListContainer}>
       {todos.length === 0 && <h2>Todo list is empty</h2>}
       {/*or we can use (!todo.lenght) */}
       {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} />
+        <Todo
+          key={todo.id}
+          todo={todo}
+          deleteTodo={deleteTodo}
+          toggleTodo={toggleTodo}
+        />
       ))}
     </div>
   );
