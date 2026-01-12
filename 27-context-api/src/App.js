cc1f52393ed99b7f00e93e157e843360b84262fa
Anastasiia Akhtiamova <aantonishen@gmail.com>
2026-01-12 16:34:27 +0100

    created project with context api

---
+++ 27-context-api/src/App.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import UserContext from "./context/UserContext";
+import "./App.css";
+import User from "./components/User";
+
+function App() {
+  const [user, setUser] = useState("Bogdan");
+  return (
+    <UserContext.Provider value={user}>
+      <div className="App">
+        <User />
+      </div>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
---
+++ 27-context-api/src/components/UserInfo.jsx
@@ -0,0 +1,8 @@
+import UserContext from "../context/UserContext";
+
+function UserInfo() {
+  return (
+    <UserContext.Consumer>{(value) => <h1>{value}</h1>}</UserContext.Consumer>
+  );
+}
+export default UserInfo;
