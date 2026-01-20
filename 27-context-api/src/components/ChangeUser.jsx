8a67fb7f04b09404385422ecf491b39afd159f58
Anastasiia Akhtiamova <aantonishen@gmail.com>
2026-01-20 14:36:52 +0100

    hook useContex

---
+++ 27-context-api/src/components/ChangeUser.jsx
@@ -0,0 +1,13 @@
+import UserContext from "../context/UserContext";
+import { useContext } from "react";
+
+function ChangeUser() {
+  const { user, setUser } = useContext(UserContext);
+  return (
+    <button onClick={() => setUser(user === "Bogdan" ? "Alice" : "Bogdan")}>
+      Change user
+    </button>
+  );
+}
+
+export default ChangeUser;
